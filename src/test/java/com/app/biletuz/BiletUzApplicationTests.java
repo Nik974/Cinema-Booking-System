a1a9f21b192f340e3783d3b44470a849fc14cf3d
Mikita Prozarau <68516115+Nik974@users.noreply.github.com>
2025-11-15 00:01:31 +0100

    Add initial contextLoads test

---
+++ src/test/java/com/app/biletuz/BiletUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.biletuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiletUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
