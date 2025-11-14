b7e230f30d287d4305fe80db99daf0b31c285e64
Mikita Prozarau <68516115+Nik974@users.noreply.github.com>
2025-11-14 23:16:32 +0100

    Add application properties

---
+++ src/main/java/com/app/biletuz/BiletUzApplication.java
@@ -0,0 +1,13 @@
+package com.app.biletuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiletUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiletUzApplication.class, args);
+    }
+
+}
